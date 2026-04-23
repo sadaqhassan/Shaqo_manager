b1d8651e8309453ae444a01be6ff740787bd1ee6
sadaqhassan <abdallaabdurahman845@gmail.com>
2026-04-23 20:49:44 +0300

    first setup

---
+++ Client/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+
+function App() {
+  return (
+    <div>
+
+      <Routes>
+        <Route
+      </Routes>
+    </div>
+  )
+}
+
+export default App
